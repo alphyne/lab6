f521abf2b03cf1ff7b6fc23b8a62ef5ee878496f
Jared Suasin <jsuasin@ucsd.edu>
2018-02-15 10:45:44 -0800

    Finish lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,13 +10,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	$('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -26,5 +27,22 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	var project = this;
+	$.get('http://localhost:3000/project/' + idNumber, function
+		callback(result){
+
+		console.log(result);
+		addProject(projectID, result);
+	})
+
+	console.log("User clicked on project " + idNumber, projectID, idNumber);
+}
+
+function addProject(id, result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>' + result['summary']; 
+  $('#'+id).find('.details').html(projectHTML)
 }
